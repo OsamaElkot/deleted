728975d4210ecc7fb94527ea73e51b840c712e92
OsamaElkot <52540403+OsamaElkot@users.noreply.github.com>
2019-07-05 09:31:24 +0200

    Update MapsActivity.java

--- app/src/main/java/com/smsm/deleted/MapsActivity.java
+++ app/src/main/java/com/smsm/deleted/MapsActivity.java
@@ -22,7 +22,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        // hello
     }
 
 
